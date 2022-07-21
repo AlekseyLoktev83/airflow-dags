e9733f1035515a3e72131ad4fae0c4b6eceef737
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-21 13:49:53 +0300

    Create azure_blob_test.py

---
+++ azure_blob_test.py
@@ -0,0 +1,20 @@
+import os
+from datetime import datetime
+
+from airflow.models import DAG
+from airflow.providers.microsoft.azure.operators.wasb_delete_blob import WasbDeleteBlobOperator
+from airflow.providers.microsoft.azure.transfers.local_to_wasb import LocalFilesystemToWasbOperator
+
+PATH_TO_UPLOAD_FILE = os.environ.get('AZURE_PATH_TO_UPLOAD_FILE', 'example-text.txt')
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+DAG_ID = "azure_blob_test"
+
+with DAG(
+    DAG_ID,
+    schedule_interval="@once",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    default_args={"container_name": "russiapetcarejupiter", "blob_name": "InterfaceTest"},
+) as dag:
+    upload = LocalFilesystemToWasbOperator(task_id="upload_file", file_path=PATH_TO_UPLOAD_FILE)
+
