b235fb5290944915b8998226f579891e0fa0506f
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-22 10:35:38 +0300

    Create parallel_task_check.py

---
+++ parallel_task_check.py
@@ -0,0 +1,46 @@
+import datetime
+import pendulum
+
+from airflow import DAG, XComArg
+from airflow.decorators import dag, task
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.providers.odbc.hooks.odbc import OdbcHook
+from airflow.providers.apache.hdfs.hooks.webhdfs import WebHDFSHook
+from airflow.providers.yandex.operators.yandexcloud_dataproc import  DataprocCreatePysparkJobOperator
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.hooks.base_hook import BaseHook
+from airflow.providers.hashicorp.hooks.vault import VaultHook
+
+import uuid
+from io import StringIO
+import urllib.parse
+import subprocess
+
+import cloud_scripts.mssql_scripts as mssql_scripts
+import json
+import pandas as pd
+import glob
+import os
+import csv
+
+@Task
+def gen_input_list():
+  return [1,2,3]
+
+@Task
+def process_item(item):
+  print(f'item={item}')
+
+with DAG(
+    dag_id='parallel_task_check',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["jupiter", "dev"],
+    render_template_as_native_obj=True,
+) as dag:
+  process_item.expand(item=gen_input_list())
