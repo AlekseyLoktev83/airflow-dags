8f109f7e58b407b08a86420d6ad120b02792ba61
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-25 08:57:27 +0300

    Update mssql_hdfs.py

--- mssql_hdfs.py
+++ mssql_hdfs.py
@@ -14,7 +14,7 @@ with DAG(
 ) as dag:
     download_table = BashOperator(
         task_id='download_table',
-        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "SELECT [ID],REPLACE(REPLACE([DTO],CHAR(10),'\'),CHAR(13),'\') [DTO],REPLACE(REPLACE([ROLE],CHAR(10),'\'),CHAR(13),'\') [ROLE],REPLACE(REPLACE([FIELDS],CHAR(10),'\'),CHAR(13),'\') [FIELDS],[STAT_ID],[STAMP],[INTERNALID],[ADDITIONFIELDS],[REMOVEDATE] , (SELECT count(*) FROM dbo.[EDITABLEPRESETS]) [#QCCount] FROM dbo.[EDITABLEPRESETS]" /JUPITER/RAW/2022/07/22/dbo/EDITABLEPRESETS/FULL/EDITABLEPRESETS.csv "-S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1" 0x01 dbo "IDDTOROLEFIELDSSTAT_IDSTAMPINTERNALIDADDITIONFIELDSREMOVEDATE#QCCount" ",
+        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh \"SELECT [ID],REPLACE(REPLACE([DTO],CHAR(10),'\'),CHAR(13),'\') [DTO],REPLACE(REPLACE([ROLE],CHAR(10),'\'),CHAR(13),'\') [ROLE],REPLACE(REPLACE([FIELDS],CHAR(10),'\'),CHAR(13),'\') [FIELDS],[STAT_ID],[STAMP],[INTERNALID],[ADDITIONFIELDS],[REMOVEDATE] , (SELECT count(*) FROM dbo.[EDITABLEPRESETS]) [#QCCount] FROM dbo.[EDITABLEPRESETS]\" /JUPITER/RAW/2022/07/22/dbo/EDITABLEPRESETS/FULL/EDITABLEPRESETS.csv \"-S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1\" 0x01 dbo \"IDDTOROLEFIELDSSTAT_IDSTAMPINTERNALIDADDITIONFIELDSREMOVEDATE#QCCount\" ",
         params = {'table_name':'YA_DATAMART4'},
         )
     
