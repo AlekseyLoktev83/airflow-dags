d785e668be9523daddbb5596cb711984be26ba27
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-25 10:26:49 +0300

    Update mssql_hdfs.py

--- mssql_hdfs.py
+++ mssql_hdfs.py
@@ -14,7 +14,7 @@ with DAG(
 ) as dag:
     download_table = BashOperator(
         task_id='download_table',
-        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query_local.sh && ~/exec_query_local.sh \"SELECT [ID],[SECURITYUSERID],[MERCURYUSERID],REPLACE(REPLACE([OPERATIONNAME],CHAR(10),'\'),CHAR(13),'\') [OPERATIONNAME],[STAT_ID],[STAMP],[INTERNALID],[ADDITIONFIELDS],[REMOVEDATE] , (SELECT count(*) FROM dbo.[MERCURYOPERATIONUSERLINK]) [#QCCount] FROM dbo.[MERCURYOPERATIONUSERLINK]\" /JUPITER/RAW/2022/07/22/dbo/MERCURYOPERATIONUSERLINK/FULL/MERCURYOPERATIONUSERLINK.csv \"-S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1\" 0x01 dbo \"IDSECURITYUSERIDMERCURYUSERIDOPERATIONNAMESTAT_IDSTAMPINTERNALIDADDITIONFIELDSREMOVEDATE#QCCount\" ",
+        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query_local.sh && ~/exec_query_local.sh \"SELECT [ID],[SECURITYUSERID],[MERCURYUSERID],REPLACE(REPLACE([OPERATIONNAME],CHAR(10),'\'),CHAR(13),'\') [OPERATIONNAME],[STAT_ID],[STAMP],[INTERNALID],[ADDITIONFIELDS],[REMOVEDATE] , (SELECT count(*) FROM dbo.[MERCURYOPERATIONUSERLINK]) [#QCCount] FROM dbo.[MERCURYOPERATIONUSERLINK]\" /JUPITER/RAW/2022/07/22/dbo/MERCURYOPERATIONUSERLINK/FULL/MERCURYOPERATIONUSERLINK.csv \"-S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1\" 0x01 dbo \"HDR_HDR\" ",
         params = {'table_name':'YA_DATAMART4'},
         )
     
