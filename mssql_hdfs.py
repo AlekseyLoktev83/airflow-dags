2a10591510a2557d9f2aeb428c9dadc6311fe6c2
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-25 08:55:44 +0300

    Update mssql_hdfs.py

--- mssql_hdfs.py
+++ mssql_hdfs.py
@@ -12,26 +12,12 @@ with DAG(
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
 ) as dag:
-
-    download_schema = BashOperator(
-        task_id='download_schema',
-        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/download_schema.sh && ~/download_schema.sh ",
-            )
     download_table = BashOperator(
         task_id='download_table',
-        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/download_table.sh && ~/download_table.sh {{params.table_name}} ",
+        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "SELECT [ID],REPLACE(REPLACE([DTO],CHAR(10),'\'),CHAR(13),'\') [DTO],REPLACE(REPLACE([ROLE],CHAR(10),'\'),CHAR(13),'\') [ROLE],REPLACE(REPLACE([FIELDS],CHAR(10),'\'),CHAR(13),'\') [FIELDS],[STAT_ID],[STAMP],[INTERNALID],[ADDITIONFIELDS],[REMOVEDATE] , (SELECT count(*) FROM dbo.[EDITABLEPRESETS]) [#QCCount] FROM dbo.[EDITABLEPRESETS]" /JUPITER/RAW/2022/07/22/dbo/EDITABLEPRESETS/FULL/EDITABLEPRESETS.csv "-S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1" 0x01 dbo "IDDTOROLEFIELDSSTAT_IDSTAMPINTERNALIDADDITIONFIELDSREMOVEDATE#QCCount" ",
         params = {'table_name':'YA_DATAMART4'},
         )
-    upload_file = BashOperator(
-        task_id='upload_file',
-        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/upload_file.sh && ~/upload_file.sh {{params.table_name}} ",
-        params = {'table_name':'YA_DATAMART4'},
-        )
-    query_db = BashOperator(
-        task_id='query_db',
-        bash_command='/opt/mssql-tools18/bin/sqlcmd -S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1 -C -Q "SELECT count(*) FROM dbo.YA_DATAMART4" -W -w 1024  -I',
-    )
     
-    download_schema>> download_table >> upload_file >> query_db
+
    
   
