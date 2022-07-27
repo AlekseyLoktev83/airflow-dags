44fae3b765ab605c1a22737a22e2be785fd11f5c
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-27 11:59:22 +0300

    Update dataproc_test.py

--- dataproc_test.py
+++ dataproc_test.py
@@ -30,9 +30,11 @@ with DAG(
         task_id='create_pyspark_job',
         cluster_id='c9qc9m3jccl8v7vigq10',
 #        main_python_file_uri='s3a://jupiter-app-test-storage/src/JUPITER_ROLLING_VOLUMES_HDFS_FDM.py',
-        main_python_file_uri='s3a://jupiter-app-test-storage/src/dataproc/JUPITER/MAIN.py',
+#         main_python_file_uri='s3a://jupiter-app-test-storage/src/dataproc/JUPITER/MAIN.py',
+         main_python_file_uri='hdfs:///SRC/Untitled.py',
         python_file_uris=[
-            's3a://jupiter-app-test-storage/src/dataproc/SHARED/EXTRACT_SETTING2.py',
+#             's3a://jupiter-app-test-storage/src/dataproc/SHARED/EXTRACT_SETTING2.py',
+            'hdfs:///SRC/user_func.py',
         ],
         file_uris=[
             's3a://data-proc-public/jobs/sources/data/config.json',
