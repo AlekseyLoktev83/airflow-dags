d66337c920a6a15d8881868723d4be16e9f9daa0
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-28 09:30:32 +0300

    Update jupiter_main_dispatcher.py

--- jupiter_main_dispatcher.py
+++ jupiter_main_dispatcher.py
@@ -55,7 +55,31 @@ with DAG(
         conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}","schema":SCHEMA},
         wait_for_completion = True,
         trigger_rule=TriggerRule.ONE_FAILED
-    )    
+    )
+    
+    trigger_jupiter_error_end_night_processing = TriggerDagRunOperator(
+        task_id="trigger_jupiter_error_end_night_processing",
+        trigger_dag_id="jupiter_error_end_night_processingg",  
+        conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}","schema":SCHEMA},
+        wait_for_completion = True,
+        trigger_rule=TriggerRule.ONE_FAILED
+    )
+    
+    trigger_jupiter_error_move_logs_to_blob = TriggerDagRunOperator(
+        task_id="trigger_jupiter_error_move_logs_to_blob",
+        trigger_dag_id="jupiter_error_move_logs_to_blob",  
+        conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}","schema":SCHEMA},
+        wait_for_completion = True,
+        trigger_rule=TriggerRule.ONE_FAILED
+    )
+    
+    trigger_jupiter_error_move_promo_to_archive = TriggerDagRunOperator(
+        task_id="trigger_jupiter_error_move_promo_to_archive",
+        trigger_dag_id="jupiter_error_move_promo_to_archive",  
+        conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}","schema":SCHEMA},
+        wait_for_completion = True,
+        trigger_rule=TriggerRule.ONE_FAILED
+    )        
     trigger_jupiter_start_night_processing >> trigger_jupiter_calculation_dispatcher
     trigger_jupiter_calculation_dispatcher >> trigger_jupiter_end_night_processing >> trigger_jupiter_move_logs_to_blob >> trigger_jupiter_move_promo_to_archive
-    trigger_jupiter_calculation_dispatcher >> trigger_jupiter_error_processing
+    trigger_jupiter_calculation_dispatcher >> trigger_jupiter_error_processing >> trigger_jupiter_error_end_night_processing >> trigger_jupiter_error_move_logs_to_blob >> trigger_jupiter_error_move_promo_to_archive
