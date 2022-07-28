8fc9c208c5033b9b39f020e4b35edd127e52f350
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-28 08:52:54 +0300

    Create jupiter_main_dispatcher.py

---
+++ jupiter_main_dispatcher.py
@@ -0,0 +1,30 @@
+import pendulum
+
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.bash import BashOperator
+
+SCHEMA = "Jupiter"
+
+with DAG(
+    dag_id="jupiter_calculation_dispatcher",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    schedule_interval=None,
+    tags=["jupiter", "dev"],
+) as dag:
+      trigger_jupiter_start_night_processing = TriggerDagRunOperator(
+        task_id="trigger_jupiter_start_night_processing",
+        trigger_dag_id="jupiter_start_night_processing",  
+        conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}","schema":SCHEMA},
+        wait_for_completion = True,
+    )
+    
+    trigger_jupiter_calculation_dispatcher = TriggerDagRunOperator(
+        task_id="trigger_jupiter_calculation_dispatcher",
+        trigger_dag_id="jupiter_calculation_dispatcher",  
+        conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}","schema":SCHEMA},
+        wait_for_completion = True,
+    )
+    
+    trigger_jupiter_start_night_processing >> trigger_jupiter_calculation_dispatcher
