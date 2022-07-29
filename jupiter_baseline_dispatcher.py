a81d3bf490032d0d6843a335b6b3c69bf09daba8
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-07-29 10:55:19 +0300

    Update jupiter_baseline_dispatcher.py

--- jupiter_baseline_dispatcher.py
+++ jupiter_baseline_dispatcher.py
@@ -134,7 +134,7 @@ with DAG(
     trigger_jupiter_baseline_calculation = TriggerDagRunOperator(
         task_id="trigger_jupiter_baseline_calculation",
         trigger_dag_id="jupiter_baseline_calculation",  
-        conf=dict(child_dag_config),
+        conf='{{json.loads(child_dag_config)}}'',
         wait_for_completion = True,
         trigger_rule=TriggerRule.ALL_DONE
     )  
