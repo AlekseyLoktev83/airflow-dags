652d5b6b477ada17acfccd7b0daa5684eff8def5
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-01 14:03:55 +0300

    Create ssh_test.py

---
+++ ssh_test.py
@@ -0,0 +1,17 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+
+with DAG(
+    dag_id='ssh_test',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+  SSHOperator(
+    task_id='list_ssh',ssh_conn_id='jupiter_dev_ssh',command='ls . ')
+
