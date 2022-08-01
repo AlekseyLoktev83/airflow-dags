70bd5ab73567ecee5bf8520314ba1de509a83b3f
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-01 14:30:41 +0300

    Update ssh_test.py

--- ssh_test.py
+++ ssh_test.py
@@ -5,6 +5,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.sftp.operators.sftp import SFTPOperator,SFTPOperation
 
 with DAG(
     dag_id='ssh_test',
@@ -12,6 +13,15 @@ with DAG(
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
 ) as dag:
-  ssh_task=SSHOperator(
-    task_id='list_ssh',ssh_conn_id='ssh_jupiter',command='ls . ')
+#   ssh_task=SSHOperator(
+#     task_id='list_ssh',ssh_conn_id='ssh_jupiter',command='ls . ')
+   sftp_get = SFTPOperator(task_id='sftp_get',
+                           ssh_conn_id='ssh_jupiter',
+                           operation=SFTPOperation.GET,
+                           ssh_conn_id="ssh_default",
+                           local_filepath="/tmp/YA_DATAMART4.csv",
+                           remote_filepath="/home/smartadmin/YA_DATAMART4.csv",
+                           create_intermediate_dirs=True,
+                          )
+
 
