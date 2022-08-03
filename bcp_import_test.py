d9a6054dc5b309f9e110a6bc670631e913981986
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-03 11:26:05 +0300

    Create bcp_import_test.py

---
+++ bcp_import_test.py
@@ -0,0 +1,20 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+TAGS=["jupiter", "baseline", "dev"]
+
+with DAG(
+    dag_id='bcp_import_test',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=TAGS,
+) as dag:
+    bcp_import = BashOperator(
+        task_id='bcp_import',
+        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/upload_file.sh && ~/upload_file.sh  ",
+        )
