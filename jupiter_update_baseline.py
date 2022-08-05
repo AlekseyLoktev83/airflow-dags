ba5d614ef4f9b8664bc7f233494cdfe4f77fa087
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-05 15:14:06 +0300

    Update jupiter_update_baseline.py

--- jupiter_update_baseline.py
+++ jupiter_update_baseline.py
@@ -117,6 +117,42 @@ def disable_baseline(parameters:dict):
 
     return result
 
+@task
+def update_baseline(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""exec [{schema}].[UpdateBaseline]""")
+    print(result)
+
+    return result
+
+@task
+def add_new_baseline(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""exec [{schema}].[AddNewBaseline]""")
+    print(result)
+
+    return result
+
+@task
+def enable_baseline(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""exec [{schema}].[EnableBaseLine]""")
+    print(result)
+
+    return result
+
+@task
+def truncate_temp_baseline(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""truncate table [{schema}].[TEMP_BASELINE]""")
+    print(result)
+
+    return result
+
 with DAG(
     dag_id='jupiter_update_baseline',
     schedule_interval=None,
@@ -128,9 +164,21 @@ with DAG(
 # Get dag parameters from vault    
     parameters = get_parameters()
     dis_baseline = disable_baseline(parameters)
+    truncate_temp_baseline1 = truncate_temp_baseline(parameters)
     upload_baseline = BashOperator(task_id="upload_baseline",
                                  do_xcom_push=True,
                                  bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/bcp_import.sh && ~/bcp_import.sh {{ti.xcom_pull(task_ids="get_parameters",key="BaseLineOutputPath")}}{{params.OUT_DIR}} {{ti.xcom_pull(task_ids="get_parameters",key="BcpImportParameters")}} \"{{ti.xcom_pull(task_ids="get_parameters",key="Schema")}}.TEMP_BASELINE\" "1" ',
                                  params={'OUT_DIR':BASELINE_OUTPUT_DIR},  
                                 )
-    dis_baseline >>  upload_baseline
+    up_baseline=update_baseline(parameters)
+    truncate_temp_baseline2 = truncate_temp_baseline(parameters)
+    upload_new_baseline = BashOperator(task_id="upload_new_baseline",
+                                 do_xcom_push=True,
+                                 bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/bcp_import.sh && ~/bcp_import.sh {{ti.xcom_pull(task_ids="get_parameters",key="BaseLineOutputPath")}}{{params.OUT_DIR}} {{ti.xcom_pull(task_ids="get_parameters",key="BcpImportParameters")}} \"{{ti.xcom_pull(task_ids="get_parameters",key="Schema")}}.TEMP_BASELINE\" "1" ',
+                                 params={'OUT_DIR':NEW_BASELINE_OUTPUT_DIR},  
+                                )
+    
+    add_new_bs=add_new_baseline(parameters)
+    enable_baseline = enable_baseline(parameters)
+        
+    dis_baseline >> truncate_temp_baseline1 >> upload_baseline >> up_baseline >> truncate_temp_baseline2 >> upload_new_baseline >> add_new_bs >> enable_baseline
