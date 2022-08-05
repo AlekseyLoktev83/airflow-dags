01ce33f5d95070a5911e0bd1cf98f0e6f967e184
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-05 14:15:44 +0300

    Update jupiter_update_baseline.py

--- jupiter_update_baseline.py
+++ jupiter_update_baseline.py
@@ -130,7 +130,7 @@ with DAG(
     dis_baseline = disable_baseline(parameters)
     upload_baseline = BashOperator(task_id="upload_baseline",
                                  do_xcom_push=True,
-                                 bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/bcp_import.sh && ~/bcp_import.sh {{ti.xcom_pull(task_ids="get_parameters",key="BaseLineOutputPath")}}{{params.OUT_DIR}} \"{{ti.xcom_pull(task_ids="get_parameters",key="BcpImportParameters")}}\" \"{{ti.xcom_pull(task_ids="get_parameters",key="Schema")}}.TEMP_BASELINE\" "1" ',
+                                 bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/bcp_import.sh && ~/bcp_import.sh {{ti.xcom_pull(task_ids="get_parameters",key="BaseLineOutputPath")}}{{params.OUT_DIR}} {{ti.xcom_pull(task_ids="get_parameters",key="BcpImportParameters")}} \"{{ti.xcom_pull(task_ids="get_parameters",key="Schema")}}.TEMP_BASELINE\" "1" ',
                                  params={'OUT_DIR':BASELINE_OUTPUT_DIR},  
                                 )
     dis_baseline >>  upload_baseline
