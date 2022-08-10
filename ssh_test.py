7d877ff256b7b3ef092eebc0b521ec94aa355713
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-10 11:35:22 +0300

    Update ssh_test.py

--- ssh_test.py
+++ ssh_test.py
@@ -6,6 +6,31 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.sftp.operators.sftp import SFTPOperator,SFTPOperation
+from airflow.decorators import dag, task
+from airflow.providers.apache.hdfs.hooks.webhdfs import WebHDFSHook
+from airflow.providers.ssh.hooks.ssh import SSHHook
+
+HDFS_CONNECTION_NAME = 'webhdfs_default'
+SSH_CONNECTION_NAME = 'ssh_jupiter'
+
+@task
+def copy_sftp_to_hdfs():
+    local_filepath="/tmp/YA_DATAMART4.csv",
+    remote_filepath="/home/smartadmin/YA_DATAMART4.csv",
+    dst_path = '/JUPITER/RAW/SOURCES/BASELINE/YA_DATAMART4.csv'
+    
+    ssh_hook=SSHHook(SSH_CONNECTION_NAME)
+    hdfs_hook = WebHDFSHook(HDFS_CONNECTION_NAME)
+    
+    with ssh_hook.get_conn() as ssh_client:
+     sftp_client = ssh_client.open_sftp()
+     sftp_client.get(remote_filepath, local_filepath)
+    
+    
+    conn = hdfs_hook.get_conn()
+    conn.upload(dst_path, local_filepath)
+
+    return True
 
 with DAG(
     dag_id='ssh_test',
@@ -15,12 +40,13 @@ with DAG(
 ) as dag:
 #   ssh_task=SSHOperator(
 #     task_id='list_ssh',ssh_conn_id='ssh_jupiter',command='ls . ')
-   sftp_get = SFTPOperator(task_id='sftp_get',
-                           ssh_conn_id='ssh_jupiter',
-                           operation=SFTPOperation.GET,
-                           local_filepath="/tmp/YA_DATAMART4.csv",
-                           remote_filepath="/home/smartadmin/YA_DATAMART4.csv",
-                           create_intermediate_dirs=True,
-                          )
+#    sftp_get = SFTPOperator(task_id='sftp_get',
+#                            ssh_conn_id='ssh_jupiter',
+#                            operation=SFTPOperation.GET,
+#                            local_filepath="/tmp/YA_DATAMART4.csv",
+#                            remote_filepath="/home/smartadmin/YA_DATAMART4.csv",
+#                            create_intermediate_dirs=True,
+#                           )
+   copy_sftp_to_hdfs()
 
 
