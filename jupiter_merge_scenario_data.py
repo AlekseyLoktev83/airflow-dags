0eb0d3d9b609b5c1443b9b86f2495fb0e383359e
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-24 10:34:44 +0300

    Update jupiter_merge_scenario_data.py

--- jupiter_merge_scenario_data.py
+++ jupiter_merge_scenario_data.py
@@ -39,7 +39,7 @@ AVAILABILITY_ZONE_ID = 'ru-central1-b'
 S3_BUCKET_NAME_FOR_JOB_LOGS = 'jupiter-app-test-storage'
 BCP_SEPARATOR = '0x01'
 CSV_SEPARATOR = '\u0001'
-TAGS=["jupiter", "rolling", "dev"]
+TAGS=["jupiter", "scenario", "dev"]
 BASELINE_ENTITY_NAME='BaseLine'
 PARAMETERS_FILE = 'PARAMETERS.csv'
 
