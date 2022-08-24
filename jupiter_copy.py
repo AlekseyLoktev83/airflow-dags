8e53c1b8df1cf85db751d174c0b17e66cb040de5
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-24 09:12:53 +0300

    Update and rename jupiter_copy_pipe.py to jupiter_copy.py

--- jupiter_copy.py
+++ jupiter_copy.py
@@ -280,7 +280,7 @@ def update_last_upload_date(last_upload_date):
 
 
 with DAG(
-    dag_id='jupiter_copy_pipe',
+    dag_id='jupiter_copy',
     schedule_interval=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
