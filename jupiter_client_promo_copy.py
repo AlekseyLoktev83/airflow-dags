6077020a0d5efcc14e7895d3a1bb0f09ca21df0d
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-25 14:15:34 +0300

    Create jupiter_client_promo_copy.py

---
+++ jupiter_client_promo_copy.py
@@ -0,0 +1,182 @@
+import datetime
+import pendulum
+
+from airflow import DAG, XComArg
+from airflow.decorators import dag, task
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.providers.odbc.hooks.odbc import OdbcHook
+from airflow.providers.apache.hdfs.hooks.webhdfs import WebHDFSHook
+from cloud_scripts.custom_dataproc import  DataprocCreatePysparkJobOperator
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.hooks.base_hook import BaseHook
+from airflow.providers.hashicorp.hooks.vault import VaultHook
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+import uuid
+from io import StringIO
+import urllib.parse
+import subprocess
+
+import cloud_scripts.mssql_scripts as mssql_scripts
+import json
+import pandas as pd
+import glob
+import os
+
+import struct
+from contextlib import closing
+
+
+MSSQL_CONNECTION_NAME = 'odbc_jupiter'
+HDFS_CONNECTION_NAME = 'webhdfs_default'
+VAULT_CONNECTION_NAME = 'vault_default'
+AVAILABILITY_ZONE_ID = 'ru-central1-b'
+S3_BUCKET_NAME_FOR_JOB_LOGS = 'jupiter-app-test-storage'
+BCP_SEPARATOR = '0x01'
+CSV_SEPARATOR = '\u0001'
+TAGS=["jupiter", "promo", "dev"]
+PARAMETERS_FILE = 'PARAMETERS.csv'
+
+def separator_convert_hex_to_string(sep):
+    sep_map = {'0x01':'\x01'}
+    return sep_map.get(sep, sep)
+
+@task(multiple_outputs=True)
+def get_parameters(**kwargs):
+    ti = kwargs['ti']
+    ds = kwargs['ds']
+    dag_run = kwargs['dag_run']
+    parent_process_date = dag_run.conf.get('parent_process_date')
+    process_date = parent_process_date if parent_process_date else ds
+    execution_date = kwargs['execution_date'].strftime("%Y/%m/%d")
+    parent_run_id = dag_run.conf.get('parent_run_id')
+    run_id = urllib.parse.quote_plus(parent_run_id) if parent_run_id else urllib.parse.quote_plus(kwargs['run_id'])
+    
+    schema = dag_run.conf.get('schema')
+    upload_date = kwargs['logical_date'].strftime("%Y-%m-%d %H:%M:%S")
+    file_name = dag_run.conf.get('FileName')
+    create_date = dag_run.conf.get('CreateDate')
+
+    raw_path = Variable.get("RawPath")
+    process_path = Variable.get("ProcessPath")
+    output_path = Variable.get("OutputPath")
+    white_list = Variable.get("WhiteList",default_var=None)
+    black_list = Variable.get("BlackList",default_var=None)
+    upload_path = f'{raw_path}/{execution_date}/'
+    system_name = Variable.get("SystemName")
+    last_upload_date = Variable.get("LastUploadDate")
+    
+    db_conn = BaseHook.get_connection(MSSQL_CONNECTION_NAME)
+    bcp_parameters = '-S {} -d {} -U {} -P {}'.format(db_conn.host, db_conn.schema, db_conn.login, db_conn.password)
+    
+    extract_schema = dag_run.conf.get('extract_schema')
+    client_prefix = dag_run.conf.get('client_prefix') 
+    client_name = dag_run.conf.get('client_name')
+    drop_files_if_errors = dag_run.conf.get('drop_files_if_errors')    
+    copy_mode = dag_run.conf.get('copy_mode')
+    source_path = dag_run.conf.get('source_mode')
+    
+    parent_handler_id = dag_run.conf.get('parent_handler_id')   
+    handler_id = parent_handler_id if parent_handler_id else str(uuid.uuid4())
+    timestamp_field = pendulum.now().strftime("%Y%m%d%H%M%S")
+    
+    client_promo_dir = Variable.get("ClientPromoDir")
+    upload_path = f'{raw_path}/{ client_promo_dir}/{client_prefix}_{timestamp_field}/'   
+
+    parameters = {"RawPath": raw_path,
+                  "ProcessPath": process_path,
+                  "OutputPath": output_path,
+                  "WhiteList": white_list,
+                  "BlackList": black_list,
+                  "MaintenancePathPrefix":"{}{}{}_{}_".format(raw_path,"/#MAINTENANCE/",process_date,run_id),
+                  "BcpParameters": bcp_parameters,
+                  "UploadPath": upload_path,
+                  "RunId":run_id,
+                  "SystemName":system_name,
+                  "LastUploadDate":last_upload_date,
+                  "CurrentUploadDate":upload_date,
+                  "ProcessDate":process_date,
+                  "MaintenancePath":"{}{}".format(raw_path,"/#MAINTENANCE/"),
+                  "Schema":schema,
+                  "UploadPath": upload_path,
+                  "ParentRunId":parent_run_id,
+                  "ExtractSchema":extract_schema,
+                  "ClientPrefix":client_prefix,
+                  "ClientName":client_name,
+                  "DropFilesIfErrors":drop_files_if_errors,
+                  "CopyMode":copy_mode,
+                  "HandlerId":handler_id,
+                  "TimestampField":timestamp_field,
+                  "SourcePath": source_path,
+                  }
+    print(parameters)
+    return parameters
+
+@task
+def create_child_dag_config(parameters:dict):
+    conf={"parent_run_id":parameters["ParentRunId"],"parent_process_date":parameters["ProcessDate"],"schema":parameters["Schema"]}
+    return conf
+
+@task
+def update_parameters(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""exec [{schema}].[UpdatePromo]""")
+    print(result)
+
+    return result
+
+@task
+def update_promoproductscorrection_table(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""exec [{schema}].[UpdatePromoProductsCorrection]""")
+    print(result)
+
+    return result
+  
+def set_client_upload_processing_flag_up(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""exec [{schema}].[DLSetClientUploadFlag] @Prefix = ? ,@Name = ?,  @Flag = ? """, parameters=(parameters["ClientPrefix"],parameters["ClientName"], 1))
+    print(result)
+
+    return result 
+
+def create_client_upload_wait_handler(parameters:dict):
+    odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
+    schema = parameters["Schema"]
+    result = odbc_hook.run(sql=f"""exec [{schema}].[DLCreateClientUploadWaitHandler] @Prefix = ? ,@HandlerId = ? """, parameters=(parameters["ClientPrefix"],parameters["HandlerId"]))
+    print(result)
+
+    return result   
+
+with DAG(
+    dag_id='jupiter_client_promo_copy',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=TAGS,
+    render_template_as_native_obj=True,
+) as dag:
+# Get dag parameters from vault    
+    parameters = get_parameters()
+    
+    child_dag_config = create_child_dag_config(parameters)
+    
+    set_client_upload_processing_flag_up = set_client_upload_processing_flag_up(parameters)
+    create_client_upload_wait_handler = create_client_upload_wait_handler(parameters)
+    
+#     trigger_jupiter_update_promo_copy = TriggerDagRunOperator(
+#         task_id="trigger_jupiter_update_promo_copy",
+#         trigger_dag_id="jupiter_update_promo_copy",  
+#         conf='{{ti.xcom_pull(task_ids="create_child_dag_config")}}',
+#         wait_for_completion = True,
+#     )
+    
+    child_dag_config >> set_client_upload_processing_flag_up >> create_client_upload_wait_handler
