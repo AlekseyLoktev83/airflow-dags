26e4bc4be4d655f4cea35afcdafeaf32d7e76065
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-25 09:18:50 +0300

    Update jupiter_interface_dispatcher.py

--- jupiter_interface_dispatcher.py
+++ jupiter_interface_dispatcher.py
@@ -4,8 +4,13 @@ from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.operators.bash import BashOperator
 from airflow.utils.trigger_rule import TriggerRule
+import uuid
 
 TAGS = ["jupiter", "dev","interface"]
+
+@task(task_id='generate_handler_id')
+def generate_handler_id():
+    return str(uuid.uuid4())
 
 with DAG(
     dag_id="jupiter_interface_dispatcher",
@@ -15,12 +20,15 @@ with DAG(
 #     schedule_interval='20 7 * * *',
     tags=TAGS,
 ) as dag:
+    handler_id=generate_handler_id()
+    
     trigger_jupiter_incoming_file_collect = TriggerDagRunOperator(
         task_id="trigger_jupiter_incoming_file_collect",
-        trigger_dag_id="jupiter_incoming_file_collect",  
+        trigger_dag_id="jupiter_incoming_file_collect",
+        conf={"parent_run_id":"parent_handler_id":"{{ti.xcom_pull(task_ids='generate_handler_id')}}"},
         wait_for_completion = True,
     )
     
-    trigger_jupiter_incoming_file_collect
+    handler_id >> trigger_jupiter_incoming_file_collect
     
    
