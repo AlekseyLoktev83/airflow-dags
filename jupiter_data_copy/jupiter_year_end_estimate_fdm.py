e1aab3a366148b31359b55910f2960454a603d9e
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-29 14:56:38 +0300

    Update jupiter_year_end_estimate_fdm.py

--- jupiter_data_copy/jupiter_year_end_estimate_fdm.py
+++ jupiter_data_copy/jupiter_year_end_estimate_fdm.py
@@ -164,11 +164,6 @@ def update_output_monitoring_failure(parameters:dict):
 def update_output_monitoring_success(parameters:dict):
     _update_output_monitoring(parameters,True)
     
-copy_output_data_to_db = BashOperator(task_id="copy_output_data_to_db",
-                                 do_xcom_push=True,
-                                 bash_command='/utils/bcp_import.sh {{ti.xcom_pull(task_ids="get_parameters",key="EntityOutputDir")}} {{ti.xcom_pull(task_ids="get_parameters",key="BcpImportParameters")}} \"{{ti.xcom_pull(task_ids="get_parameters",key="Schema")}}.YEAR_END_ESTIMATE_FDM\" "1" ',
-                                )
-
 @task
 def truncate_table(parameters:dict):
     odbc_hook = OdbcHook(MSSQL_CONNECTION_NAME)
@@ -212,7 +207,7 @@ with DAG(
     
     copy_output_data_to_db = BashOperator(task_id="copy_output_data_to_db",
                                  do_xcom_push=True,
-                                 bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/bcp_import.sh && ~/bcp_import.sh {{ti.xcom_pull(task_ids="get_parameters",key="EntityOutputDir")}} {{ti.xcom_pull(task_ids="get_parameters",key="BcpImportParameters")}} \"{{ti.xcom_pull(task_ids="get_parameters",key="Schema")}}.YEAR_END_ESTIMATE_FDM\" "1" ',
+                                 bash_command='/utils/bcp_import.sh {{ti.xcom_pull(task_ids="get_parameters",key="EntityOutputDir")}} {{ti.xcom_pull(task_ids="get_parameters",key="BcpImportParameters")}} \"{{ti.xcom_pull(task_ids="get_parameters",key="Schema")}}.YEAR_END_ESTIMATE_FDM\" "1" ',
                                 )
     
     cleanup = BashOperator(
