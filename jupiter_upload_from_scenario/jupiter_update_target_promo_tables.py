2a46daa0be65ac863eb732d84455c56b9661f2b1
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-08-31 10:08:58 +0300

    Update jupiter_update_target_promo_tables.py

--- jupiter_upload_from_scenario/jupiter_update_target_promo_tables.py
+++ jupiter_upload_from_scenario/jupiter_update_target_promo_tables.py
@@ -81,7 +81,7 @@ def get_parameters(**kwargs):
     db_conn = BaseHook.get_connection(MSSQL_CONNECTION_NAME)
     bcp_parameters = '-S {} -d {} -U {} -P {}'.format(db_conn.host, db_conn.schema, db_conn.login, db_conn.password)
 
-    mask_secret("BcpParameters") 
+    mask_secret(MSSQL_CONNECTION_NAME) 
     parameters = {"RawPath": raw_path,
                   "ProcessPath": process_path,
                   "OutputPath": output_path,
