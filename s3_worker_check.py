e817c19c00d070bef70f4ca55bbb5eb0fd985783
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-01 17:06:28 +0300

    Update s3_worker_check.py

--- s3_worker_check.py
+++ s3_worker_check.py
@@ -5,15 +5,20 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.decorators import dag, task
 import uuid
 
+@ask
+def gen_file_name():
+    return str(uuid.uuid4())
 
 with DAG(
-    dag_id='s3_worker_check',
+    dag_id='s3_mount_check',
     schedule_interval=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
 ) as dag:
+  gen_file_name = gen_file_name()  
   list_dir = BashOperator(
         task_id='list_dir',
         bash_command="ls -la /tmp/data/src/ ",
@@ -21,6 +26,6 @@ with DAG(
   
   create_file = BashOperator(
         task_id='create_file',
-        bash_command="touch /tmp/data/src/check{{str(pendulum.now())}} ",
+        bash_command="touch /tmp/data/src/check{{gen_file_name)}} ",
       )  
-  
+  gen_file_name >> list_dir >> create_file
