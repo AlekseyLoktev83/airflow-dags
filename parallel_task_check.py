aba4867efd479a1e5315dc68281b3e8963504325
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-02 16:32:18 +0300

    Update parallel_task_check.py

--- parallel_task_check.py
+++ parallel_task_check.py
@@ -28,7 +28,7 @@ import os
 import csv
 
 def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
+    """Yield successive n-sized chunks from lst. """
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
