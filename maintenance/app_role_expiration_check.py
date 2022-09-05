5552fcdade3bf72130c1eac5b155a9f2fa50395e
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-05 14:28:06 +0300

    Update app_role_expiration_check.py

--- maintenance/app_role_expiration_check.py
+++ maintenance/app_role_expiration_check.py
@@ -31,7 +31,7 @@ VAULT_CONNECTION_NAME = 'vault_default'
 def get_app_role_info():
     vault_hook = VaultHook(VAULT_CONNECTION_NAME)
     conn = vault_hook.get_conn()
-    resp = conn.auth.approle.read_role(
+    resp = conn.auth.approle.list_secret_id_accessors(
     role_name='airflow-role',
                   )
     print(str(resp))
