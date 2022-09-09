228fc9f9fc104c5f2fce07f9e5848cc672a39ac7
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-09 13:01:05 +0300

    Create hive_job_test.py

---
+++ hive_job_test.py
@@ -0,0 +1,39 @@
+import os
+import uuid
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.yandex.operators.yandexcloud_dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocCreateHiveJobOperator,
+    DataprocCreateMapReduceJobOperator,
+    DataprocCreatePysparkJobOperator,
+    DataprocCreateSparkJobOperator,
+    DataprocDeleteClusterOperator,
+)
+AVAILABILITY_ZONE_ID = 'ru-central1-c'
+S3_BUCKET_NAME_FOR_JOB_LOGS = ''
+
+
+
+with DAG(
+    'hive_job_test',
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    tags=['example'],
+) as dag:
+    create_hive_query = DataprocCreateHiveJobOperator(
+        task_id='create_hive_query',
+        query='SELECT 1;',
+    )
+
+    create_hive_query_from_file = DataprocCreateHiveJobOperator(
+        task_id='create_hive_query_from_file',
+        query_file_uri='s3a://data-proc-public/jobs/sources/hive-001/main.sql',
+        script_variables={
+            'CITIES_URI': 's3a://data-proc-public/jobs/sources/hive-001/cities/',
+            'COUNTRY_CODE': 'RU',
+        },
+    )
+
+    
