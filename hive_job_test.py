1c44ad3ed14d3601633e61711796f79c5bca62c9
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-09 13:45:09 +0300

    Update hive_job_test.py

--- hive_job_test.py
+++ hive_job_test.py
@@ -31,7 +31,7 @@ with DAG(
     create_hive_query_from_file = DataprocCreateHiveJobOperator(
         task_id='create_hive_query_from_file',
         cluster_id='c9qc9m3jccl8v7vigq10',
-        query='SELECT * FROM monitoring_output',
+        query='insert into monitoring_output select * from  monitoring_output',
 #         script_variables={
 #             'CITIES_URI': 's3a://data-proc-public/jobs/sources/hive-001/cities/',
 #             'COUNTRY_CODE': 'RU',
