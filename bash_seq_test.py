ce9b6a8b26b922fe1b43b89afeebd4d1bf1c7757
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-12 09:40:55 +0300

    Create bash_seq_test.py

---
+++ bash_seq_test.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.hooks.base import BaseHook
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from airflow.configuration import ensure_secrets_loaded
+from airflow.providers.hashicorp.secrets.vault import VaultBackend
+from airflow.providers.hashicorp.hooks.vault import VaultHook
+import time
+    
+
+with DAG('bash_seq_test', start_date=datetime(2022, 1, 1), schedule_interval=None) as dag:
+  ping = BashOperator(
+        task_id='ping',
+        bash_command="echo 1;echo 2;echo 3 ",
+            )
