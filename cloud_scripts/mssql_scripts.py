c28c5536535f23a32f0861e3198e5a7f37bc5832
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-12 10:35:53 +0300

    Update mssql_scripts.py

--- cloud_scripts/mssql_scripts.py
+++ cloud_scripts/mssql_scripts.py
@@ -144,3 +144,62 @@ def get_first(odbc_hook, sql, parameters=None):
             columns = [column[0] for column in cur.description]
             row = cur.fetchone()
             return dict(zip(columns, row))
+        
+def mip_generate_table_select_query(current_upload_date, last_upload_date, actual_schema_file, delta_tables = []):
+    df = pd.read_csv(actual_schema_file, keep_default_na=False,sep=CSV_SEPARATOR)
+    rows = df.to_dict('records')
+    grouped_rows = {i: list(j) for (i, j) in groupby(
+        rows, lambda x: (x["Schema"], x["TableName"]))}
+
+    result = []
+    for table, columns in grouped_rows.items():
+        print(table)
+        print()
+        fields_list = []
+        column_name_list = []
+        for column in columns:
+            if column["FieldType"] == 'nvarchar':
+                fields_list.append("REPLACE(REPLACE([{field_name}],CHAR(10),''),CHAR(13),'') [{field_name}]".format(
+                    field_name=column["FieldName"]))
+            elif column["FieldType"] == 'numeric' and column["Size"] == 38:
+                fields_list.append("CONVERT(NVARCHAR(40),[{field_name}]) [{field_name}]".format(
+                    field_name=column["FieldName"]))
+            elif column["FieldType"] == 'datetimeoffset' and column["FieldName"] != 'STAMP':
+                fields_list.append("""IIF(DATEPART(YEAR, [{field_name}])<1900,
+                IIF(DATEPART(TZ, [{field_name}])<0,
+                        DATEADD(HOUR, -3, CONVERT(DATETIME,DATEADD(YEAR, 1900-DATEPART(YEAR, [{field_name}]), [{field_name}]),0)),
+                            CONVERT(DATETIME,DATEADD(YEAR, 1900-DATEPART(YEAR, [{field_name}]), [{field_name}]),0)),
+                            IIF(DATEPART(TZ, [{field_name}])<0,DATEADD(HOUR, -3,
+                             CONVERT(DATETIME,[{field_name}],0)),CONVERT(DATETIME,[{field_name}],1))) [{field_name}]""".format(field_name=column["FieldName"]))
+            elif column["FieldType"] == 'datetimeoffset' and column["FieldName"] == 'STAMP':
+                fields_list.append("CONVERT(DATETIME,[{field_name}],1) [{field_name}]".format(
+                    field_name=column["FieldName"]))
+            else:
+                fields_list.append("[{field_name}]".format(
+                    field_name=column["FieldName"]))
+
+            column_name_list.append(column["FieldName"])        
+
+        fields = ",".join(fields_list)
+
+        column_name_list.append("#QCCount")
+        column_names = ",".join(column_name_list)
+
+
+
+        method = METHOD_FULL
+        if table[1] in delta_tables:
+            method = METHOD_DELTA
+
+            script = "SELECT {fields} , (SELECT count(*) FROM {schema}.[{table_name}] WHERE LastModifiedDate BETWEEN CONVERT(nvarchar(20),'{last_modified_date}', 120) AND CONVERT(nvarchar(20),'{current_upload_date}', 120)) [#QCCount] FROM {schema}.[{table_name}] t WHERE t.LastModifiedDate BETWEEN CONVERT(nvarchar(20),'{last_modified_date}', 120) AND CONVERT(nvarchar(20),'{current_upload_date}', 120)".format(
+                fields=fields, schema=table[0], table_name=table[1], last_modified_date=last_upload_date, current_upload_date=current_upload_date)
+        else:
+            script = "SELECT {fields} , (SELECT count(*) FROM {schema}.[{table_name}]) [#QCCount] FROM {schema}.[{table_name}]".format(
+                fields=fields, schema=table[0], table_name=table[1])
+
+        result.append(
+            {"Schema": table[0], "EntityName": table[1], "Extraction": script, "Method": method, "Columns":column_names})
+
+    result_df = pd.DataFrame(result)
+    return result_df
+        
