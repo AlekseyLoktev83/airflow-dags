e34ae0b587f14d6d70ce4cb71af27d11334fd6c4
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-13 10:36:06 +0300

    Update db_to_s3_test.py

--- db_to_s3_test.py
+++ db_to_s3_test.py
@@ -26,6 +26,6 @@ with DAG(
   
   export_to_s3 = BashOperator(
         task_id='export_to_s3',
-        bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query_s3.sh && ~/exec_query_s3.sh  "SELECT [NAME],[INDEX],[INDEXES],[ID],[METHA_ID],[TYPE_ID],[CALC_TYPE_ID],[KLS_ID],[KLS_FIELD_ID],[UNIQUE_ID],[STAT_ID] , (SELECT count(*) FROM dbo.[METHA_ATTR]) [#QCCount] FROM dbo.[METHA_ATTR]" /tmp/data/METHA_ATTR.csv "-S 192.168.10.39 -d MIP_Prod_20220415 -U userdb -P qwerty1" 0x01 dbo "NAME\x01INDEX\x01INDEXES\x01ID\x01METHA_ID\x01TYPE_ID\x01CALC_TYPE_ID\x01KLS_ID\x01KLS_FIELD_ID\x01UNIQUE_ID\x01STAT_ID\x01#QCCount" ',
+        bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query_s3.sh && ~/exec_query_s3.sh  "SELECT [NAME],[INDEX],[INDEXES],[ID],[METHA_ID],[TYPE_ID],[CALC_TYPE_ID],[KLS_ID],[KLS_FIELD_ID],[UNIQUE_ID],[STAT_ID] , (SELECT count(*) FROM dbo.[METHA_ATTR]) [#QCCount] FROM dbo.[METHA_ATTR]" /tmp/data/src/METHA_ATTR.csv "-S 192.168.10.39 -d MIP_Prod_20220415 -U userdb -P qwerty1" 0x01 dbo "NAME\x01INDEX\x01INDEXES\x01ID\x01METHA_ID\x01TYPE_ID\x01CALC_TYPE_ID\x01KLS_ID\x01KLS_FIELD_ID\x01UNIQUE_ID\x01STAT_ID\x01#QCCount" ',
       )  
   export_to_s3
