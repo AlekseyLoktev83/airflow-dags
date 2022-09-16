e2035211fac40cd4423b631d5c30a07d101de78b
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-16 13:43:48 +0300

    Update mip_raw_data_upload_chunks.py

--- mip/mip_raw_data_upload_chunks.py
+++ mip/mip_raw_data_upload_chunks.py
@@ -220,7 +220,7 @@ def start_monitoring_detail(dst_dir, upload_path, runid, entities):
 def end_monitoring_detail(dst_dir, entities):
     hdfs_hook = WebHDFSHook(HDFS_CONNECTION_NAME)
     conn = hdfs_hook.get_conn()
-    print(str(entities))
+    print(str(list(entities)))
 
     result = []
     for ent in list(entities):
