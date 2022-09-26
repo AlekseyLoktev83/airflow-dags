aacb84978c5fa4fdc99d77220223578f30dc8a9c
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-26 16:10:21 +0300

    Update jupiter_main_dispatcher.py

--- jupiter_calculation/jupiter_main_dispatcher.py
+++ jupiter_calculation/jupiter_main_dispatcher.py
@@ -88,8 +88,9 @@ with DAG(
         conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}","schema":SCHEMA},
         wait_for_completion = True,
         trigger_rule=TriggerRule.ONE_FAILED
-    )        
+    )
+    handler_id >> trigger_jupiter_start_night_processing >> trigger_jupiter_calculation_dispatcher >> trigger_jupiter_calculation_dispatcher
 #     Success branch
-    handler_id >> trigger_jupiter_start_night_processing >> trigger_jupiter_calculation_dispatcher >> trigger_jupiter_calculation_dispatcher >> trigger_jupiter_end_night_processing >> trigger_jupiter_move_logs_to_sftp >> trigger_jupiter_move_promo_to_archive
+    trigger_jupiter_end_night_processing >> trigger_jupiter_move_logs_to_sftp >> trigger_jupiter_move_promo_to_archive
 #     Error branch
-    handler_id >> trigger_jupiter_start_night_processing >> trigger_jupiter_calculation_dispatcher >> trigger_jupiter_calculation_dispatcher >> trigger_jupiter_error_processing >> trigger_jupiter_error_end_night_processing >> trigger_jupiter_error_move_logs_to_sftp >> trigger_jupiter_error_move_promo_to_archive
+    trigger_jupiter_error_processing >> trigger_jupiter_error_end_night_processing >> trigger_jupiter_error_move_logs_to_sftp >> trigger_jupiter_error_move_promo_to_archive
