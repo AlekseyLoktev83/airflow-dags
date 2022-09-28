2656ca76fbdd962a978e3d011e409f42571ad029
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-28 11:25:51 +0300

    Create postgres_test.py

---
+++ postgres_test.py
@@ -0,0 +1,24 @@
+# this is not production code. just useful for testing connectivity.
+from airflow.providers.odbc.hooks.odbc import OdbcHook
+from airflow.providers.apache.hdfs.hooks.webhdfs import WebHDFSHook
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.hooks.base_hook import BaseHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+import base64
+
+dag = DAG(
+    dag_id="postgres_test",
+    default_args={},
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=["example"],
+    catchup=False,
+)
+
+    create_table = PostgresOperator(
+        task_id='query_1',
+        postgres_conn_id="postgres_default",
+        sql='''select 1;''',
+    )
