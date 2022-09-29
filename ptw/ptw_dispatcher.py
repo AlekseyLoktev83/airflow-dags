adec83a48ffbbf3e76f12b55cc42851476b9fb43
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-09-29 09:44:00 +0300

    Create ptw_dispatcher.py

---
+++ ptw/ptw_dispatcher.py
@@ -0,0 +1,28 @@
+import pendulum
+
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="ptw_dispatcher",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    schedule_interval=None,
+    tags=["ptw", "dev"],
+) as dag:
+   
+    trigger_dag_raw_upload = TriggerDagRunOperator(
+        task_id="trigger_raw_data_upload",
+        trigger_dag_id="ptw_raw_data_upload",  
+        conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}"},
+        wait_for_completion = True,
+    )
+    trigger_dag_qc = TriggerDagRunOperator(
+        task_id="trigger_raw_qc",
+        trigger_dag_id="ptw_raw_qc",  
+        conf={"parent_run_id":"{{run_id}}","parent_process_date":"{{ds}}"},
+        wait_for_completion = True,
+    )
+    
+    trigger_dag_raw_upload >> trigger_dag_qc
