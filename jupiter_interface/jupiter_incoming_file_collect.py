fc247298f15e2b6d8e44c20569bb1a658e00180a
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-10-31 12:31:28 +0300

    Update jupiter_incoming_file_collect.py

--- jupiter_interface/jupiter_incoming_file_collect.py
+++ jupiter_interface/jupiter_incoming_file_collect.py
@@ -191,7 +191,7 @@ with DAG(
     create_file_info = create_file_info(parameters=parameters, entity=get_intermediate_file_metadata)
     copy_file_to_target_folder = BashOperator(task_id="copy_file_to_target_folder",
                                        do_xcom_push=True,
-                                      bash_command=create_file_info["CopyCommand"],
+                                      bash_command=XComArg(create_file_info["CopyCommand"]),
                                               )
     add_filebuffer_sp = add_filebuffer_sp(parameters=parameters, entity=create_file_info)
     
