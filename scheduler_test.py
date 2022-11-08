a84926281afb84d50ad524f2bde2724226c59237
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-11-08 09:54:05 +0300

    Create scheduler_test.py

---
+++ scheduler_test.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from airflow.models import Variable
+from airflow.hooks.base_hook import BaseHook
+from airflow.decorators import dag, task
+
+ 
+@task
+def task2():
+  print(2)  
+
+with DAG('scheduler_test',
+         start_date=datetime(2022, 11, 8),
+         schedule_interval='5 15 * * *',
+         default_args=default_args,
+         catchup=False,
+        ) as dag:
+      task2
+    
