8602fb2d12fe91e57e1c840dd8f1d66cb4344f0b
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-11-29 12:19:19 +0300

    Update jupiter_atlas_copy_azure.py

--- jupiter_data_copy/jupiter_atlas_copy_azure.py
+++ jupiter_data_copy/jupiter_atlas_copy_azure.py
@@ -110,7 +110,7 @@ def generate_distcp_script(parameters:dict, entity):
     export AZCOPY_SPA_CLIENT_SECRET={azure_conn.password}
     export AZCOPY_TENANT_ID={azure_conn.extra_dejson['extra__azure__tenantId']}
     
-    azcopy copy {src_path} /tmp/entity --recursive && hdfs dfs -put -f /tmp/entity/*/* {dst_path}{entity_subfolder} && rm -rf /tmp/entity   
+    azcopy copy {src_path} /tmp/entity --recursive && hdfs dfs -rm -f {dst_path}{entity_subfolder} && hdfs dfs -put -f /tmp/entity/*/* {dst_path}{entity_subfolder} && rm -rf /tmp/entity   
     
     """
 
@@ -124,7 +124,7 @@ def generate_entity_list(parameters:dict):
               {'SrcPath':'https://marsanalyticsprodadls.dfs.core.windows.net/output/ATLAS/AEP/MASTER_DATA/0CUSTOMER_ATTR/DATA','DstPath':dst_dir},
               {'SrcPath':'https://marsanalyticsprodadls.dfs.core.windows.net/output/ATLAS/AEP/MASTER_DATA/0CUSTOMER_TEXT/DATA','DstPath':dst_dir},
               {'SrcPath':'https://marsanalyticsprodadls.dfs.core.windows.net/output/ATLAS/AEP/MASTER_DATA/0CUST_SALES_ATTR/DATA','DstPath':dst_dir},
-              {'SrcPath':'https://marsanalyticsprodadls.dfs.core.windows.net/output/ATLAS/AEP/MASTER_DATA/0CUST_SALES_LKDH_HIER_T_ELEMENTS/DATA','DstPath':dst_dir},
+              {'SrcPath':'https://marsanalyticsprodadls.dfs.core.windows.net/output/ATLAS/AEP/HIERARCHY/0CUST_SALES_LKDH_HIER_T_ELEMENTS/DATA','DstPath':dst_dir},
               {'SrcPath':'https://marsanalyticsprodadls.dfs.core.windows.net/output/ATLAS/AEP/MASTER_DATA/0CUST_SALES_TEXT/DATA','DstPath':dst_dir},
               {'SrcPath':'https://marsanalyticsprodadls.dfs.core.windows.net/output/ATLAS/AEP/MASTER_DATA/ZCUSTOPF_ATTR/DATA','DstPath':dst_dir},
              ]
