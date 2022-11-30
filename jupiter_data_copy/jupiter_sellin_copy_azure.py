36839deb4fec4b45c903cfb4e5854017d5efbf69
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-11-30 09:50:57 +0300

    Update jupiter_sellin_copy_azure.py

--- jupiter_data_copy/jupiter_sellin_copy_azure.py
+++ jupiter_data_copy/jupiter_sellin_copy_azure.py
@@ -71,7 +71,7 @@ def get_parameters(**kwargs):
     azure_conn = BaseHook.get_connection(AZURE_CONNECTION_NAME)
     print(azure_conn)
     
-    dst_dir = f'{raw_path}/SOURCES_REMOTE/SELLIN/'
+    dst_dir = f'{raw_path}/SOURCES/SELLIN/'
     
 
     parameters = {"RawPath": raw_path,
@@ -119,8 +119,8 @@ def generate_entity_list(parameters:dict):
 
 with DAG(
     dag_id='jupiter_sellin_copy_azure',
-    schedule_interval=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    schedule_interval='0 20 * * *',
+    start_date=pendulum.datetime(2022, 11, 29, tz="UTC"),
     catchup=False,
     tags=TAGS,
     render_template_as_native_obj=True,
