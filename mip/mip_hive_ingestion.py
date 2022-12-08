4dc7ed0e423d6247c6c6f41c5885766016363c45
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-08 12:24:32 +0300

    Update mip_hive_ingestion.py

--- mip/mip_hive_ingestion.py
+++ mip/mip_hive_ingestion.py
@@ -6,7 +6,7 @@ from airflow.decorators import dag, task
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.providers.odbc.hooks.odbc import OdbcHook
 from airflow.providers.apache.hdfs.hooks.webhdfs import WebHDFSHook
-from cloud_scripts.custom_dataproc import  DataprocCreatePysparkJobOperator
+from cloud_scripts.custom_dataproc import  DataprocCreateHiveJobOperator
 from airflow.models import Variable
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
