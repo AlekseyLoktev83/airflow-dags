8b5fe638c3fb8a3bf3be43e8371f24d2d66e6e4f
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-22 13:02:34 +0300

    Update kafka_test.py

--- kafka/kafka_test.py
+++ kafka/kafka_test.py
@@ -22,6 +22,14 @@ connection_config = {
     "ssl.ca.location": "/tmp/YandexCA.crt"
 }
 
+def consumer_function(message, prefix=None):
+    key = json.loads(message.key())
+    value = json.loads(message.value())
+    consumer_logger.info(
+        f"{prefix} {message.topic()} @ {message.offset()}; {key} : {value}"
+    )
+    return
+
 with DAG(
     dag_id="kafka_test",
     start_date=datetime(2022, 11, 1),
@@ -41,3 +49,25 @@ with DAG(
         producer_function=producer_function, 
         kafka_config=connection_config
     )
+    
+    consumer_task = ConsumeFromTopicOperator(
+        task_id="consume_from_topic",
+        topics=["jupiter"],
+        apply_function=functools.partial(consumer_function, prefix="consumed:::"),
+        consumer_config={
+            "bootstrap.servers": "rc1b-0qd2fn83sq9vp78r.mdb.yandexcloud.net:9091",
+            "security.protocol": "SASL_SSL",
+            "sasl.mechanism": "SCRAM-SHA-512",
+            "sasl.username": "jupiter-user",
+            "sasl.password": "pass1234",
+             "ssl.ca.location": "/tmp/YandexCA.crt",
+            "group.id": "foo",
+            "enable.auto.commit": False,
+            "auto.offset.reset": "beginning",
+        },
+        commit_cadence="end_of_batch",
+        max_messages=30,
+        max_batch_size=10,
+    )
+    
+    producer_task >> consumer_task
