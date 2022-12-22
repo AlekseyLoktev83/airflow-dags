15130b8f7a9485ea6de10ee1b5b4b8a63c1e91d4
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-22 11:01:45 +0300

    Create kafka_test.py

---
+++ kafka/kafka_test.py
@@ -0,0 +1,42 @@
+# kafka_example_dag_1.py 
+
+import os
+import json
+import logging
+import functools
+from pendulum import datetime
+
+from airflow import DAG
+from airflow_provider_kafka.operators.produce_to_topic import ProduceToTopicOperator
+
+# get the topic name from .env
+my_topic = "jupiter"
+
+# get Kafka configuration information
+connection_config = {
+    "bootstrap.servers": "rc1b-0qd2fn83sq9vp78r.mdb.yandexcloud.net:9092",
+    "security.protocol": "SASL_PLAINTEXT",
+    "sasl.mechanism": "PLAIN",
+    "sasl.username": "jupiter-user",
+    "sasl.password": "pass1234"
+}
+
+with DAG(
+    dag_id="kafka_test",
+    start_date=datetime(2022, 11, 1),
+    schedule=None,
+    catchup=False,
+):
+
+    # define the producer function
+    def producer_function():
+        for i in range(5):
+            yield (json.dumps(i), json.dumps(i+1))
+
+    # define the producer task
+    producer_task = ProduceToTopicOperator(
+        task_id=f"produce_to_{my_topic}",
+        topic=my_topic,
+        producer_function=producer_function, 
+        kafka_config=connection_config
+    )
