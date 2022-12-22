d452e833b98474264bf0e7d7dc9181e9ff64553d
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-22 13:06:57 +0300

    Update kafka_test.py

--- kafka/kafka_test.py
+++ kafka/kafka_test.py
@@ -56,17 +56,12 @@ with DAG(
         topics=["jupiter"],
         apply_function=functools.partial(consumer_function, prefix="consumed:::"),
         consumer_config={
-            "bootstrap.servers": "rc1b-0qd2fn83sq9vp78r.mdb.yandexcloud.net:9091",
-            "security.protocol": "SASL_SSL",
-            "sasl.mechanism": "SCRAM-SHA-512",
-            "sasl.username": "jupiter-user",
-            "sasl.password": "pass1234",
-             "ssl.ca.location": "/tmp/YandexCA.crt",
             "group.id": "foo",
             "enable.auto.commit": False,
             "auto.offset.reset": "beginning",
         },
         commit_cadence="end_of_batch",
+        kafka_config=connection_config,
         max_messages=30,
         max_batch_size=10,
     )
