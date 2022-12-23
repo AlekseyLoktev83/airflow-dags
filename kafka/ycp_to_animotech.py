14c96575948f4aca0719ac79848d433b4d6c8345
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-23 16:24:41 +0300

    Update ycp_to_animotech.py

--- kafka/ycp_to_animotech.py
+++ kafka/ycp_to_animotech.py
@@ -135,11 +135,9 @@ def generate_entity_list(parameters:dict):
     
     return entities
 
-def producer_function(ag):
-    print(ag)
-#     entities=context['ti'].xcom_pull(task_ids='generate_entity_list')
-#     for e in list(entities):
-#         yield (e)
+def producer_function(entities=None):
+    for e in list(entities):
+        yield (e)
 
 with DAG(
     dag_id='ycp_to_animotech',
@@ -162,7 +160,7 @@ with DAG(
         task_id=f"produce_to_{my_topic}",
         topic=my_topic,
         producer_function=producer_function,
-        producer_function_args="ARG1",
+        producer_function_kwargs={"entities": generate_entity_list},
         kafka_config=connection_config,
         do_xcom_push=True,
     )
