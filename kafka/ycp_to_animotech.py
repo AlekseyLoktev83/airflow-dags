6a449644a7558601743638dacd872fe0062bf714
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-23 13:18:27 +0300

    Update ycp_to_animotech.py

--- kafka/ycp_to_animotech.py
+++ kafka/ycp_to_animotech.py
@@ -160,7 +160,7 @@ with DAG(
         task_id=f"produce_to_{my_topic}",
         topic=my_topic,
         producer_function=producer_function,
-        producer_function_args= XComArg(generate_entity_list),
+        producer_function_args=generate_entity_list,
         kafka_config=connection_config
     )
     
