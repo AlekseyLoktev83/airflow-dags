abea0aabee4faa9b68c158223f03ad1443c16ac3
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-23 09:21:08 +0300

    Update kafka_test.py

--- kafka/kafka_test.py
+++ kafka/kafka_test.py
@@ -9,6 +9,7 @@ from pendulum import datetime
 from airflow import DAG
 from airflow_provider_kafka.operators.produce_to_topic import ProduceToTopicOperator
 from airflow_provider_kafka.operators.consume_from_topic import ConsumeFromTopicOperator
+from airflow_provider_kafka.operators.await_message import AwaitKafkaMessageOperator
 
 # get the topic name from .env
 my_topic = "jupiter"
@@ -32,6 +33,10 @@ def consumer_function(message, prefix=None):
         f"{prefix} {message.topic()} @ {message.offset()}; {key} : {value}"
     )
     return
+
+def await_function(message):
+    if json.loads(message.value()) % 5 == 0:
+        return f" Got the following message: {json.loads(message.value())}"
 
 with DAG(
     dag_id="kafka_test",
@@ -72,4 +77,22 @@ with DAG(
         max_batch_size=10,
     )
     
-    consumer_task
+    await_task = AwaitKafkaMessageOperator(
+        task_id="awaiting_message",
+        topics=["jupiter"],
+        apply_function="await_function",
+        kafka_config={
+                "bootstrap.servers": "rc1b-0qd2fn83sq9vp78r.mdb.yandexcloud.net:9091",
+                "security.protocol": "SASL_SSL",
+                 "sasl.mechanism": "SCRAM-SHA-512",
+                 "sasl.username": "jupiter-user",
+                 "sasl.password": "pass1234",
+                 "ssl.ca.location": "/tmp/YandexCA.crt", 
+            "group.id": "awaiting_message",
+            "enable.auto.commit": False,
+            "auto.offset.reset": "beginning",
+        },
+        xcom_push_key="retrieved_message",
+    )    
+    
+    consumer_task>>await_task
