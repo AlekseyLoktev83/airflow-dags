b613261f784be7ab2f31cdc200ab0c753c5b1d50
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-23 11:12:56 +0300

    Update kafka_scripts.py

--- cloud_scripts/kafka_scripts.py
+++ cloud_scripts/kafka_scripts.py
@@ -8,8 +8,8 @@ def generate_hdfs_to_s3_copy_file_command(s3_connection_name, filename, src_path
     
     
     script = f'''
-    hdfs dfs -get {src_path} /tmp/{filename} && s3cmd put /tmp/{filename} {dst_path} --host=storage.yandexcloud.net --access_key={s3_conn.login} --secret_key={s3_conn.password} --host-bucket="%(bucket)s.storage.yandexcloud.net" && rm -rf /tmp/{filename}   
-    
+    hdfs dfs -get {src_path} /tmp/{filename} && s3cmd put /tmp/{filename} {dst_path} --host=storage.yandexcloud.net --access_key={s3_conn.login} --secret_key={s3_conn.password} --host-bucket="%(bucket)s.storage.yandexcloud.net" && rm -rf /tmp/{filename}    
+    && echo "{dst_path}{filename}"
     '''
 
     return script
