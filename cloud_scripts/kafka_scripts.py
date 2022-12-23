cdf8143f0f2dd8b7a3af63579e99d1b3e8b77819
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-12-23 10:51:17 +0300

    Update kafka_scripts.py

--- cloud_scripts/kafka_scripts.py
+++ cloud_scripts/kafka_scripts.py
@@ -4,11 +4,11 @@ from airflow.hooks.base_hook import BaseHook
 def generate_hdfs_to_s3_copy_file_command(s3_connection_name, filename, src_path, dst_path):
     entity_file = os.path.basename(src_path)
     
-    s3_connection_name = BaseHook.get_connection(s3_connection_name)
+    s3_conn = BaseHook.get_connection(s3_connection_name)
     
     
     script = f'''
-    hdfs dfs -get {src_path} /tmp/{filename} && s3cmd put /tmp/{filename} {dst_path} --host=storage.yandexcloud.net --access_key={azure_conn.login} --secret_key={azure_conn.password} --host-bucket="%(bucket)s.storage.yandexcloud.net" && rm -rf /tmp/{filename}   
+    hdfs dfs -get {src_path} /tmp/{filename} && s3cmd put /tmp/{filename} {dst_path} --host=storage.yandexcloud.net --access_key={s3_conn.login} --secret_key={s3_conn.password} --host-bucket="%(bucket)s.storage.yandexcloud.net" && rm -rf /tmp/{filename}   
     
     '''
 
